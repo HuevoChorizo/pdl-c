efb0b22c8d7fc03b668d639c0a12b042b7d13c91
HuevoChorizo <jjimenezvinuesa@gmail.com>
2025-10-23 15:49:59 +0200

    Prueba de abrir fichero y leer

--- src/main.c
+++ src/main.c
@@ -1,7 +1,27 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-/* El propósito de este código es comenzar el git y e;l proyecto como tal */
+int main() {
+  int fd = open("TokensEntrada", O_RDONLY);
+  int size;
+  char *buf;
+  if (fd == -1) {
+    printf("Error al abrir el fichero");
+    exit(1);
+  }
 
-int main() { return 0; }
+  /*Aquí deberíamos implementar el lector directamente, hay que hacer un struct
+   * que desarrolle las propiedades de los tokens (eventualmente xd ) */
+  size = lseek(fd, 0, SEEK_END);
+  lseek(fd, 0, SEEK_SET);
+  read(fd, &buf, size);
+  printf("%s", buf);
+
+  if (close(fd) != 0) {
+    printf("El fichero se ha cerrado con un error.");
+    return -1;
+  }
+  return 0;
+}
