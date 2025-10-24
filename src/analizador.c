64829f7e1028e49e2ea85303648acfc11bebb4e9
HuevoChorizo <jjimenezvinuesa@gmail.com>
2025-10-24 19:22:09 +0200

    Flipalo

--- src/analizador.c
+++ src/analizador.c
@@ -1,5 +1,6 @@
 #include "tokens.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /*Esto básicamente se podría pasar a otro fichero, por ejemplo comprobar.c, pero
  * de momento, como estoy probando no me apetece.*/
@@ -22,17 +23,44 @@ int analizador(char **desglosado) {
       tokens[j].id_pal = desglosado[i];
       j++;
     }
+
     /*Esto quizás era más preciso hacerlo en lector, pero llevaría más tiempo,
      * pues recorrería todos los caracteres de forma innecesaria analizandolos,
      * y si ya hay una agrupación que se puede definir como un token, o una
      * palabra reservada de primeras, perderíamos tiempo.*/
     int k = 0;
     while (desglosado[i][k] != '\0') {
+      int encontrado = 0;
       char aux = desglosado[i][k];
-      char aux2 = desglosado[i][k + 1];
-      if (aux == '(' || aux == ')' || aux == '"' ||
-          (aux == '/' && aux2 == '*') || (aux == '*' &&))
-        k++;
+      char aux1 = desglosado[i][k + 1];
+      if ((aux == '/' && aux1 == '*') || (aux == '*' && aux1 == '/') ||
+          (aux == '%' && aux1 == '=') || (aux == '&' && aux1 == '&') ||
+          (aux == '|' && aux1 == '|') || (aux == '=' && aux1 == '=')) {
+        char *aux2 = malloc(3 * sizeof(char));
+        aux2[0] = aux;
+        aux2[1] = aux1;
+        aux2[2] = '\0';
+        tokens[j].id_pal = aux2;
+        encontrado = 1;
+        free(aux2);
+      } else if (aux == '=' || aux == ',' || aux == ';' || aux == '(' ||
+                 aux == ')' || aux == '{' || aux == '}' || aux == '+' ||
+                 aux == '*' || aux == '/' || aux == '!' || aux == '<' ||
+                 aux == '>') {
+        char *aux2 = malloc(2 * sizeof(char));
+        aux2[0] = aux;
+        aux2[1] = '\0';
+        tokens[j].id_pal = aux2;
+        free(aux2);
+        encontrado = 1;
+      }
+      int l = 0;
+      while (encontrado) {
+
+        encontrado = 0;
+      }
+
+      k++;
     }
   }
 
