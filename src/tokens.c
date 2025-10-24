6088ae8c1099e2d7c18237801535476871dc114a
AdriChachipistach1 <abiancaadri123@gmail.com>
2025-10-24 14:57:33 +0200

    modified:   src/tokens.c 	modified:   src/tokens.h

--- src/tokens.c
+++ src/tokens.c
@@ -1,19 +1,33 @@
 #include <stdlib.h>
+#include <stdio.h>
 
+//Inicializo los contadores para los valores de los atributos
+int atr_OP=0;
+int atr_PAL_RES=0;
 
-
-
+//Estructura de los tokens
 typedef struct token_s{
     char* id_pal;
     int atribute;
 }token;
 
-void inittoken(token* tok){
-    tok->id_pal;
-    tok->atribute;
+//El "constructor" de los tokens
+token newToken(token* tok,char* id){
+    tok->id_pal=id;
+    switch('id'){
+        case 'PAL_RES':
+        tok->atribute=atr_OP++;
+        break;
+        case 'OP' :
+        tok->atribute=atr_PAL_RES++;
+        break;
+        case 'EOF' :
+        tok->atribute=0;
+        break;
+    }
 }
 
-
+//Un toString que no se si tendrá uso la verdad pero ahí esta mal hecho.
 char* toString(token* tok){
     char* id = tok->id_pal;
     int atr= tok->atribute;
@@ -30,4 +44,13 @@ char* toString(token* tok){
     res[i+3]=' ';
     res=realloc(res,sizeof(char)*(i+3)+sizeof(int));
     res[i+4]=atr;
+    printf(res);
 }
+
+int estok_valido(token* aux_t){
+    if(aux_t->id_pal==('PAL_RES'||'OP'||'EOF'))
+        return 1;
+    return 0;
+}
+
+
--- src/tokens.h
+++ src/tokens.h
@@ -1,4 +1,16 @@
+#ifndef TOKENS_C
+#define TOKENS_C
 
+typedef struct token_s{
+    char* id_pal;
+    int atribute;
+}token;
+
+token newToken(token* tok,char* id);
+char* toString(token* tok);
+int estok_valido(token* tok);
+
+#endif  
 
 
 
