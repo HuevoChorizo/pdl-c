78d0f0e2348e2e8b27046382e7004914501170cd
HuevoChorizo <jjimenezvinuesa@gmail.com>
2025-10-26 11:15:20 +0100

    Modificar token y procesador

--- src/procesador.c
+++ src/procesador.c
@@ -90,13 +90,7 @@ char **procesador(char **desglosado) {
 
     i++;
   }
-  i = 0;
-  while (procesado[i] != NULL) {
-    printf("Token %d: %s\n", i + 1, procesado[i]);
-    i++;
-  }
 
   procesado[j] = NULL;
-  procesado = realloc(procesado, (j + 1) * sizeof(char *));
   return procesado;
 }
